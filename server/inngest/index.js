60ac90dd4dc7d65ea7d44eed06947c407da18783
TaMeeM-Gamol <tamemgamol7@gmail.com>
2026-04-27 00:52:17 +0300

    connect mongodb in inngest functions

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import User from "../models/user.js";
+import connectDB from "../configs/db.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -8,6 +9,7 @@ export const inngest = new Inngest({ id: "pingup-app" });
 const syncUserCreation = inngest.createFunction(
   {id:'sync-user-from-clerk', triggers: [{event: 'clerk/user.created'}]},
   async ({event}) => {
+      await connectDB();
     const { id, first_name, last_name, email_addresses, image_url } = event.data
     let username = email_addresses[0].email_address.split('@')[0]
 
@@ -35,6 +37,7 @@ const syncUserCreation = inngest.createFunction(
 const syncUserUpdation = inngest.createFunction(
   {id:'update-user-from-clerk', triggers: [{event: 'clerk/user.updated'}]},
   async ({event}) => {
+      await connectDB();
     const { id, first_name, last_name, email_addresses, image_url } = event.data
 
     const updateUserData = {
@@ -51,6 +54,7 @@ const syncUserUpdation = inngest.createFunction(
 const syncUserDeletion = inngest.createFunction(
   {id:'delete-user-with-clerk', triggers: [{event: 'clerk/user.deleted'}]},
   async ({event}) => {
+      await connectDB();
     const { id } = event.data
     await User.findByIdAndDelete(id)
   }
